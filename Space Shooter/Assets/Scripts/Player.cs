3cf7768549947fd6d976bfb48b1834e1892a9c22
Nemanja Cvetkovic <nemanjacvetkovic1993@yahoo.com>
2021-04-09 23:31:36 +0200

    NEW: Player Laser Fire

--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    // Configuration Parameters
     [SerializeField] float moveSpeed = 12f;
     [SerializeField] float spaceShipPadding = 1f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float laserSpeed = 22f;
 
     float xMin;
     float xMax;
@@ -19,20 +21,13 @@ public class Player : MonoBehaviour
         SetUpMoveBoundaries();
         
     }
-
-    private void SetUpMoveBoundaries()
-    {
-        Camera gameCamera = Camera.main;
-        // This is for the bottom left screen, only first value (x) is important.
-        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + spaceShipPadding;
-        // This is for the bottom right screen, with value of 1.
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - spaceShipPadding;
-    }
+ 
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Fire();
     }
 
     private void Move()
@@ -41,6 +36,23 @@ public class Player : MonoBehaviour
         var movingXPosition = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var newXPosition = Mathf.Clamp(transform.position.x + movingXPosition, xMin, xMax);
         transform.position = new Vector2(newXPosition, transform.position.y);
+    }
+
+    private void Fire()
+    {
+        if(Input.GetButtonDown("Fire1")) {
+            GameObject laser =  Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, laserSpeed);
+        }
+    }
+    
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        // This is for the bottom left screen, only first value (x) is important.
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + spaceShipPadding;
+        // This is for the bottom right screen, with value of 1.
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - spaceShipPadding;
     }
 }
 
