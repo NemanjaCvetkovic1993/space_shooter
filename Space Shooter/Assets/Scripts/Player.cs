b9453a442f25088d410ef965aed2569abbb9105b
Nemanja Cvetkovic <nemanjacvetkovic1993@yahoo.com>
2021-04-09 19:26:36 +0200

    NEW: player.cs X movemen

--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// my third commit
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] float moveSpeed = 12f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        // Time.deltaTime makes every computer to work the same, regardless of their FPS
+        var movingXPosition = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPosition = transform.position.x + movingXPosition;
+        transform.position = new Vector2(newXPosition, transform.position.y);
     }
 }
